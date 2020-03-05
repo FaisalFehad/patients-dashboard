5ce3f5eb2370c24fa4de49800512f664652e0dac
faisal <faisal@localhost.x230>
2020-03-05 17:24:35 +0000

    InfoBar component

---
+++ patients-dashboard-fe/src/components/InfoBar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class componentName extends Component {
+  render() {
+    return (
+      <header>
+        <nav class="navbar navbar-light ">
+          <div class="container-fluid">
+            <div class="page-name">
+              <h5>Patients</h5>
+              <div class="details">
+                Total
+                <br />
+                1401/2392 Found
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default componentName;
